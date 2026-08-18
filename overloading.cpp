a16b1ca45c98fb4d2efd17f044f4c4b8f05af085
Vikas Yadav <vikas142yadav@gmail.com>
2026-08-18 10:28:57 +0530

    overloading

--- overloading.cpp
+++ overloading.cpp
@@ -1,40 +1,35 @@
 #include <iostream>
 using namespace std;
-void sum();
-int sum(int, int);
-float sum(int, int, float);
+class Overload
+{
+    int a, b;
+
+public:
+    void sum();
+    int sum(int, int);
+    // double sum(int, double);
+};
+void Overload::sum()
+{
+    cout << "Enter the numbers: ";
+    cin >> a >> b;
+    int s = a + b;
+    cout << "The sum is: " << s;
+}
+int Overload::sum(int x, int y)
+{
+    a = x;
+    b = y;
+    int s = a + b;
+    return (s);
+}
 int main()
 {
-    int a, b, r1;
-    float r2, c;
-    cout << "sum function without parameters: "<<endl;
-    sum(); // first function calling
-    cout << " sum function with 2 parameters: "<<endl;
-    cout << "Enter the two numbers: "<<endl;
-    cin >> a >> b;
-    r1 = sum(a, b);
-    cout << " The sum is :" << r1<<endl;
-    cout << " sum function with 3 parameters: "<<endl;
-    cout << " Enter the three numbers: "<<endl;2
-    cin >> a >> b >> c;
-    r2 = sum(a, b, c);
-    cout << " The sum is :" << r2<<endl;
-}
-void sum()
-{
-    int x, y, s;
-    cout << "Enter the numbers: "<<endl;
-    cin >> x >> y;
-    s = x + y;
-    cout << "The sum is: " << s<<endl;
-}
-int sum(int x, int y)
-{
-    int s = x + y;
-    return s;
-}
-float sum(int x, int y, float z)
-{
-    float s = x + y + z;
-    return (s);
+    int n1, n2;
+    Overload O1;
+    O1.sum();
+    cout << "\nEnter the numbers: ";
+    cin >> n1 >> n2;
+    int r = O1.sum(n1, n2);
+    cout << "\nThe sum is: " << r;
 }
